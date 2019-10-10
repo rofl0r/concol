0606bb9dc37a3057f361a617086a6f62e4445993
rofl0r <rofl0r@users.noreply.github.com>
2019-10-10 21:27:55 +0100

    sdlconsole: make msleep() static for namespace cleanness
    otherwise it would collide with e.g. libulz.

--- sdlconsole.c
+++ sdlconsole.c
@@ -75,7 +75,7 @@ void console_clear(Console* self) {
 #include <time.h>
 #include <errno.h>
 
-int msleep(long millisecs) {
+static int msleep(long millisecs) {
         struct timespec req, rem;
         req.tv_sec = millisecs / 1000;
         req.tv_nsec = (millisecs % 1000) * 1000 * 1000;
